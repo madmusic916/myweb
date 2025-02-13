afa2d78d08001278a9b7819dc380fef2c02eeadc
madmusic916 <madmusic916@gmail.com>
2025-02-13 09:50:40 +0530

    Delete firebaseConfig.js

--- firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyCWf9xkJPcwHUjS71s2vpNiGIljcxux_Tw",
-    authDomain: "digibyte-ffa4e.firebaseapp.com",
-    projectId: "digibyte-ffa4e",
-    storageBucket: "digibyte-ffa4e.appspot.com",
-    messagingSenderId: "1061576583718",
-    appId: "1:1061576583718:web:0760674758d4e7787594e7",
-    measurementId: "G-526R70KJ6Q"
-};
